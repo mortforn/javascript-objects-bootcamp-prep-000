eeb24d5c566cbc115bcc8718ac4100706f7bfd25
Matthew Fine <mfine@wesleyan.edu>
2018-09-08 01:29:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,3 @@
 var playlist = {Radiohead: "Karma Police"};
 
-function updatePlayList(obj, key, value) {
-  return object.assign({}, playlist, {[Slayer]: "Raining Blood"})
-}
+playlist['Slayer'] = "Raining Blood";
